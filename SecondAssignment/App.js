0714fc1fcedfea45e5337d53d4aa0e1458a59a4d
unknown <Ashhar@Fahad-Kamran.FAHAD-KAMRAN>
2023-04-11 15:38:21 +0500

    finalize second assignment

--- SecondAssignment/App.js
+++ SecondAssignment/App.js
@@ -9,86 +9,100 @@ let student = {
 }
 
 // QUESTION # 01
-
-// function myClosures(num1) {
-//     return function (num2) {
-//         return num1 + num2;
-//     }
-// }
-
-// let sum = myClosures(5);
-// console.log('Q # 1 ===>', sum(6));
+// **************************
+function myClosures(num1) {
+    return function (num2) {
+        return num1 + num2;
+    }
+}
+let sum = myClosures(5);
+console.log('Q # 1 ===>', sum(6));
+// **************************
 
 // QUESTION # 02
-
-// let dummyArr = ['faizan', 'shayan', 'anas', 'zubair', 'fahad', 'hilal', 'junai', 'asad', 'ishaq', 'bhojani'];
-// let count = 0;
-// const findFunction = (str) => {
-//     console.log('fdsa')
-// }
-// findFunction('asad');
+// **************************
+let dummyArr = ['faizan', 'shayan', 'anas', 'zubair', 'fahad', 'hilal', 'junai', 'asad', 'ishaq', 'bhojani'];
+let count = 0;
+function findFunction(str) {
+    //  debugger
+    if (count < dummyArr.length) {
+        if (dummyArr[count] == str) {
+            return true;
+        }
+        else {
+            count ++
+            return findFunction(str)
+        }
+    }
+    else {
+        return false;
+    }
+}
+console.log('Q # 2',findFunction('bhojani'));
+// **************************
 
 // QUESTION # 03
+// **************************
+const addPara = (str) => {
+    // create new element <p></p>
+    let newPara = document.createElement('p');
+    let newParaText = document.createTextNode(str);
+    newPara.appendChild(newParaText);
 
-//  const addPara = (str) => {
-
-//     // create new element <p></p>
-//     let newPara = document.createElement('p');
-//     let newParaText = document.createTextNode(str);
-//     newPara.appendChild(newParaText);
-
-//     document.body.appendChild(newPara)
-// }
-// // pass argument paragraph text
-
-// addPara('Add this paragraph from DOM manipulation');
+    document.body.appendChild(newPara)
+}
+// pass argument paragraph text
+addPara('Add this paragraph from DOM manipulation');
+// **************************
 
 // QUESTION # 04
-
-// const unorderList = (str) => {
-//     // create unorder List
-//     let myUl = document.getElementById('unorderList');
-//     // create list item
-//     let myLi = document.createElement('li');
-//     let liText = document.createTextNode(str);
-//     myLi.appendChild(liText);
-//     myUl.appendChild(myLi)
-// }
-// unorderList('Lorem ipsum, dolor sit amet consectetur adipisicing elit.');
+// **************************
+const unorderList = (str) => {
+    let myUl = document.getElementById('unorderList');  // get unordered-list
+    let myLi = document.createElement('li');   // create li
+    let liText = document.createTextNode(str); // create textNode
+    myLi.appendChild(liText);                  // asign textNode on li
+    myUl.appendChild(myLi)
+}
+unorderList('Lorem ipsum, dolor sit amet consectetur adipisicing elit.');  // call function and pass string argument
+// **************************
 
 // QUESTION # 05
-
-// const changeBgColor = (id, color) => {
-//     document.getElementById(id).style.backgroundColor = color;
-// };
-// changeBgColor('body', 'dodgerblue');
+// **************************
+const changeBgColor = (id, color) => {
+    document.getElementById(id).style.backgroundColor = color;
+};
+changeBgColor('body', 'dodgerblue');
+// **************************
 
 // QUESTION # 06
-
-// const setLocalStorage = (key, obj) => {
-//     localStorage.setItem(key, JSON.stringify(obj));
-// }
-// setLocalStorage('student', student);
+// **************************
+const setLocalStorage = (key, obj) => {
+    localStorage.setItem(key, JSON.stringify(obj));
+}
+setLocalStorage('student', student);
+// **************************
 
 // QUESTION # 07
-
-// const getLocalStorage = (key) => {
-//     console.log(localStorage.getItem(key));
-// }
-// getLocalStorage('student');
-
+// **************************
+const getLocalStorage = (key) => {
+    console.log('Q # 7 ==>', localStorage.getItem(key));
+}
+getLocalStorage('student');
+// **************************
 
 // QUESTION # 08
-
-// const setLocalData = (data) => {
-//     Object.keys(data).forEach(function (key) {
-//         localStorage.setItem(key, data[key]);
-//     });
-//     let newObj = new Object();
-//     Object.keys(data).forEach(function (key) {
-//         newObj[key] = localStorage.getItem(key)
-//     });
-//     return newObj;
-// }
-// let getNewObject = setLocalData(student);
-// console.log('new object get localstorage', getNewObject);
+// **************************
+const setLocalData = (data) => { // create function
+    Object.keys(data).forEach(function (key) {
+        localStorage.setItem(key, data[key]);
+    });
+    let newObj = new Object(); // create new object
+    Object.keys(data).forEach(function (key) {
+        newObj[key] = localStorage.getItem(key)
+    });
+    return newObj;  // return object
+}
+let getNewObject = setLocalData(student); // call function and pass object as an argument
+console.log('Q # 8 ==>', getNewObject);
+// **************************
